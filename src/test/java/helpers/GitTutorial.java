69f71f69ba4f91335b61ae634d962f5a2244e3df
Luis Miguel Alvarez <lalvarez@switch.local>
2019-04-22 12:31:46 -0300

    algunos cambios en la pagina de GitTutorial

--- src/test/java/helpers/GitTutorial.java
+++ src/test/java/helpers/GitTutorial.java
@@ -7,3 +7,26 @@ public class GitTutorial {
 	}
 
 }
+//pasos para subir cambios al repo
+ /* 1- git add *
+  * 2- git commit -m "mensaje"
+  * 3- git push origin master  
+ */
+
+//pasos para bajarse los ultimos cambios del repo
+/* 1- git pull
+ */
+
+//comprobar instalación de git (se ejecuta en la consola)
+/* 1- git --version
+ */
+//para añadir un directorio al git
+/* 1- me paro en la consola de windows en el directorio
+ * 2- git init
+ * 3- dir
+ * 4- git commit -m "mensaje"
+ * 5- git add *
+ * 6- git commit -m "mensaje"
+ * 7- git remote add origin https://(ruta del proyecto en github)
+ * 8- git push origin master
+ */
